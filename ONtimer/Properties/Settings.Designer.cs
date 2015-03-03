e71cc474f12f79f4230fb4fae7a679dea61bac4d
unknown <haverkort@PROWARE-L61.prowareness.nl>
2015-03-03 11:19:06 +0100

    Added: New feature "Always on top"

--- ONtimer/Properties/Settings.Designer.cs
+++ ONtimer/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace ONtimer.Properties {
                 this["ExpirationBlinkingEnabled"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool AlwaysOnTop {
+            get {
+                return ((bool)(this["AlwaysOnTop"]));
+            }
+            set {
+                this["AlwaysOnTop"] = value;
+            }
+        }
     }
 }
